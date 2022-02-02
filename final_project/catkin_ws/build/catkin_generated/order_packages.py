aa26bc25e10429218600983f70559b4be9a72a10
Nehal Nevle <nehalnevle@gmail.com>
2022-02-02 22:20:21 +0530

    final project

---
+++ final_project/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/nehalnevle/Localiztation/final_project/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/nehalnevle/Localiztation/final_project/catkin_ws/devel;/home/nehalnevle/ws_moveit/devel;/opt/ros/noetic'.split(';') if '/home/nehalnevle/Localiztation/final_project/catkin_ws/devel;/home/nehalnevle/ws_moveit/devel;/opt/ros/noetic' != '' else []
---
+++ final_project/catkin_ws/build/udacity_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nehalnevle/Localiztation/final_project/catkin_ws/src/udacity_bot
+# Build directory: /home/nehalnevle/Localiztation/final_project/catkin_ws/build/udacity_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
